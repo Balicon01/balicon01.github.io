5713247e84b29c1d3a8efafab1c7e93a10ca9035
Balicon01 <anton.2000halyavin@gmail.com>
2025-01-24 23:28:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
-<html lang="ru">
+<html>
 
 <head>
   <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
   <meta charset="utf-8">
-  <title>ID Eternal RP</title>
+  <title>id-gulka</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.css" />
   <style>
     body {
       margin: 0;
@@ -29,6 +28,16 @@
       font-size: 17px;
     }
 
+    .topnav a:hover {
+      background-color: #ddd;
+      color: black;
+    }
+
+    .topnav a.active {
+      background-color: #4CAF50;
+      color: white;
+    }
+
     header {
       position: fixed;
       top: 0;
@@ -37,6 +46,8 @@
       height: 88px;
       z-index: 10;
       background: #eeeeee;
+      -webkit-box-shadow: 0 7px 8px rgba(0, 0, 0, 0.12);
+      -moz-box-shadow: 0 7px 8px rgba(0, 0, 0, 0.12);
       box-shadow: 0 7px 8px rgba(0, 0, 0, 0.12);
     }
 
@@ -44,13 +55,14 @@
       margin-top: 90px;
     }
   </style>
+  <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.css" />
 </head>
 
 <body>
   <header>
     <div class="topnav text-white">
-      <button type="button" class="btn btn-primary btn-lg" id="relsi" autocomplete="off">Eternal</button>
-      <button type="button" class="btn btn-secondary btn-lg" id="relorig" autocomplete="off">Original</button>
+      <button type="button" class="btn btn-primary btn-lg" id="relsi" data-bs-toggle="button" autocomplete="off">Base SI</button>
+      <button type="button" class="btn btn-secondary btn-lg" id="relorig" data-bs-toggle="button" autocomplete="off">Original</button>
       <div class="form-group">
         <input type="text" class="form-control" id="search" placeholder="Поиск по таблице">
       </div>
@@ -63,62 +75,67 @@
           <th>Photo</th>
           <th>ID</th>
           <th>Name</th>
+          <th>Discription</th>
         </tr>
       </thead>
       <tbody id="dataTable"></tbody>
     </table>
   </div>
-
   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="https://cdn.datatables.net/2.0.8/js/jquery.dataTables.min.js"></script>
   <script>
-    $(document).ready(function () {
-      // Привязка кнопок
-      $('#relsi').click(function () {
-        Reload('https://Balicon01.github.io/3266436726.json');
-      });
+    $(document).ready(function ()
+    {
+      $('#relsi').click(
+        function ()
+        {
+          Reload('https://mrgulka.github.io/2612257810.json')
+        });
+      $("#relorig").click(
+        function ()
+        {
+          Reload('https://mrgulka.github.io/1664827613.json')
+        }
+      )
+    });
+    function Reload(path)
+    {
+      $.getJSON(path, function (data)
+      {
+        var allRecordsHTML = '';
+        for (var i = 0; i < data.length; i++) {
+          if (data[i].Type == "Object" 
+          || data[i].Type == "Vehicle"
+          || data[i].Type == "Effect" 
+          || data[i].Type == "Large"
+          || data[i].Type == "Medium"
+          || data[i].Type == "Decal") continue;
+          allRecordsHTML += '<tr>';
+          allRecordsHTML += '<td>' + '<img height="50" class="image" src="https://mrgulka.github.io/images/' + data[i].ID + '.png" oneerror=\'this.style.display = "none"\' alt=" "/>' + '</td>';
+          allRecordsHTML += '<td>' + data[i].ID + '</td>';
+          allRecordsHTML += '<td>' + data[i].Name + '</td>';
+          allRecordsHTML += '<td>' + data[i].Description + '</td>';
+          allRecordsHTML += '</tr>';
+        }
+        var table = document.getElementById("dataTable");
 
-      $('#relorig').click(function () {
-        Reload('https://Balicon01.github.io/3412516593.json');
-      });
-
-      // Функция загрузки данных
-      function Reload(path) {
-        $.getJSON(path)
-          .done(function (data) {
-            let allRecordsHTML = '';
-            const excludedTypes = ["Object", "Vehicle", "Effect", "Large", "Medium", "Decal"];
-            for (let i = 0; i < data.length; i++) {
-              if (excludedTypes.includes(data[i].Type)) continue;
-
-              allRecordsHTML += `
-                <tr>
-                  <td>
-                    <img loading="lazy" height="50" class="image" 
-                      src="https://Balicon01.github.io/images/${data[i].ID}.png" 
-                      onerror="this.style.display='none'" 
-                      data-toggle="tooltip" title="<img height='140px' src='https://Balicon01.github.io/images/${data[i].ID}.png' alt='Item'>" />
-                  </td>
-                  <td>${data[i].ID}</td>
-                  <td>${data[i].Name}</td>
-                </tr>`;
-            }
-            $('#dataTable').html(allRecordsHTML);
-            $('#display_json_data').DataTable().destroy(); // Удалить предыдущую инициализацию
-            $('#display_json_data').DataTable(); // Инициализация DataTable после контента
-            $('[data-toggle="tooltip"]').tooltip(); // Инициализация тултипов
-          })
-          .fail(function () {
-            alert('Ошибка загрузки данных. Пожалуйста, попробуйте снова.');
-          });
-      }
-
-      // Реализация поиска
-      $("#search").keyup(function () {
-        var searchText = $(this).val().toLowerCase();
-        $("#dataTable tr").each(function () {
-          $(this).toggle($(this).text().toLowerCase().indexOf(searchText) > -1);
+        table.innerHTML = allRecordsHTML;
+      })
+    };
+  </script>
+  <script>
+    $(document).ready(function ()
+    {
+      $("#search").keyup(function ()
+      {
+        _this = this;
+        $.each($("#display_json_data tbody tr"), function ()
+        {
+          if ($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1) {
+            $(this).hide();
+          }
+          else {
+            $(this).show();
+          }
         });
       });
     });
